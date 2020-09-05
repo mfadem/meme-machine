876ea8d4c5d06a04e1de42975dcf2828032cfe96
Michael Fadem <michaelfadem@outlook.com>
2020-09-05 17:53:49 -0600

    Break up the functions and start using the imgflip api, maybe

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Meme Machine
+This code is for my twitter bot: [CS Post Bot 4096](https://twitter.com/MemeMachine4096)
+
 Pure memes, pure competition to [CS Post Bot 2048](https://github.com/cgilbert250/CSPostBot2048)
+
+## Development
+* This bot was developed on WSL Ubuntu `18.04` utilizing Python `3.6.9`
+
+## Dependencies
+* Tweepy: Easily interface with Twitter API
+* Pillow: Modify/Create Images
+
+## Installation & Execution
+* Clone this repo
+* Pip install the `requirements.txt` file
+    * I reccommend using a virtual environment to keep everything kosher on your system
+* Run the code from the root of the project with `python3 source/bot.py`
--- source/bot.py
+++ source/bot.py
@@ -1,6 +1,8 @@
 # Imports for relevant libraries
-import tweepy
 import json
+import requests
+import urllib
+import tweepy
 from PIL import Image
 
 class TwitterBot:
@@ -15,41 +17,71 @@ class TwitterBot:
             self.bearer_token = dict_config["bearer-token"]
             self.access_token = dict_config["access-token"]
             self.access_token_secret = dict_config["access-token-secret"]
+            self.imageflip_username = dict_config["imageflip-username"]
+            self.imageflip_password = dict_config["imageflip-password"]
+            self.templates = []
             # print(self.api_key)
             # print(self.api_secret_key)
             # print(self.bearer_token)
             # print(self.access_token)
             # print(self.access_token_secret)
+            # print(self.imageflip_username)
+            # print(self.imageflip_password)
 
         # Authenticate the api client
         auth = tweepy.OAuthHandler(self.api_key, self.api_secret_key)
         auth.set_access_token(self.access_token, self.access_token_secret)
         self.api_client = tweepy.API(auth, wait_on_rate_limit=True)
 
-    def getTwitter(self):
+    def updateBotStatus(self, status_text=""):
+        with open('user.json', 'r') as user_json:
+            user_dict = json.load(user_json)
+        media_id = mediaUpload("images/hello_there.png")
+        self.api_client.update_status("@Science2048", media_ids=media_id)
+
+    def slideIntoDM(self):
+        with open('user.json', 'r') as user_json:
+            user_dict = json.load(user_json)
+        media_id = mediaUpload("images/hello_there.png")
+        self.api_client.send_direct_message(user_dict["id"], "", attachment_type="media", attachment_media_id=media_id)
+
+    def getData(self, user=""):
         tweets = self.api_client.home_timeline()
         for tweet in tweets:
             print(tweet.text)
-        user = self.api_client.get_user(screen_name="Science2048")
-        # user = self.api_client.get_user(screen_name="rPrequelMemes")
+        user_data = self.api_client.get_user(screen_name=user)
         with open('user.json', 'w') as user_json:
             json.dump(user._json, user_json, indent=4)
 
-    def postTwitter(self):
-        with open('user.json', 'r') as user_json:
-            user_dict = json.load(user_json)
-        # img = Image.open("images/hello_there.jpg")
-        # img.save("images/hello_there.png")
+    def mediaUpload(self, filename):
         media_upload = self.api_client.media_upload("images/hello_there.png")
         media_id = [media_upload.media_id_string]
-        # self.api_client.send_direct_message(user_dict["id"], "", attachment_type="media", attachment_media_id=media_id)
-        self.api_client.update_status("@Science2048", media_ids=media_id)
+        return media_id
+
+    def convertImagePng(self, jpg_file):
+        image = Image.open(jpg_file)
+        image_name = jpg_file.split(".")
+        image_name = image_name[0] + ".png"
+        image.save(image_name)
+
+    def pullMemeTemplates(self):
+        data = requests.get('https://api.imgflip.com/get_memes').json()['data']['memes']
+        self.templates = [{'name':image['name'],'url':image['url'],'id':image['id']} for image in data]
+
+        print('Here is the list of available memes : \n')
+        ctr = 1
+        for meme in self.templates:
+            print(ctr, meme['name'])
+            ctr = ctr+1
+
+    def createMeme(self, topText, bottomText, image=""):
+        return True
 
 
 def main():
     apiInstance = TwitterBot("source/config.json")
-    apiInstance.getTwitter()
-    apiInstance.postTwitter()
+    apiInstance.pullMemeTemplates()
+    # apiInstance.getTwitter()
 
 # Program Entry Point
 if __name__ == '__main__':
